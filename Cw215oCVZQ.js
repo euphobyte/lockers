3e7c1761156e1ffe1d9d1160e405403e9bce0fe5
euphobyte <56632888+euphobyte@users.noreply.github.com>
2021-09-11 22:43:31 +0200

    Update Cw215oCVZQ.js

--- Cw215oCVZQ.js
+++ Cw215oCVZQ.js
@@ -1,6 +1,5 @@
 function test(){
   var lb = document.createElement("H1");
-  lb.innerText = "Hello World!";
-  var x = document.getElementsByTagName("BODY")[0];
-  x.appendChild(lb);
+  lb.textContent = "Hello World!";
+  document.body.appendChild(lb);
 }
